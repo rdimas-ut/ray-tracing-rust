84c5711065c9dfa73e9b6f9759f764a62de5fede
Ruben <rdimas@utexas.edu>
2021-08-13 21:55:07 -0500

    Background render

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,9 @@ use std::ops::Div;
 
 use std::fmt;
 
+#[derive(Copy, Clone)]
 struct Vec3(f64, f64, f64);
+
 use Vec3 as Point3;
 use Vec3 as Color;
 
@@ -159,6 +161,25 @@ impl Div<f64> for Vec3 {
     }
 }
 
+struct Ray {
+    origin: Point3,
+    direction: Vec3 
+}
+
+impl Ray {
+    fn at(self, t: f64) -> Point3 {
+        self.origin + t*self.direction
+    }
+
+    fn origin(self) -> Point3 {
+        self.origin
+    }
+
+    fn direction(self) -> Vec3 {
+        self.direction
+    }
+}
+
 fn first_image() {
     // Image
     const IMAGE_WIDTH: u32 = 256;
@@ -204,6 +225,45 @@ fn second_image() {
     eprintln!("Done. ");
 }
 
+fn third_image() {
+    // Image
+    const ASPECT_RATIO: f64 = 16.0/9.0;
+    const IMAGE_WIDTH: u64 = 400;
+    const IMAGE_HEIGTH: u64 = (IMAGE_WIDTH as f64/ASPECT_RATIO) as u64;
+
+    // Camera
+    let viewport_height: f64= 2.0;
+    let viewport_width: f64 = ASPECT_RATIO * viewport_height;
+    let focal_length: f64 = 1.0;
+
+    let origin = Point3(0.0, 0.0, 0.0);
+    let horizontal: Vec3 = Vec3(viewport_width, 0.0, 0.0);
+    let vertical: Vec3 = Vec3(0.0, viewport_height, 0.0);
+    let lower_left_corner: Vec3 = origin - horizontal/2.0 - vertical/2.0 - Vec3(0.0, 0.0, focal_length);
+
+    // Render
+    print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGTH);
+
+    for j in (0..IMAGE_HEIGTH).rev() {
+        eprintln!("Scanlines remaining: {}", j);
+        for i in 0..IMAGE_WIDTH {
+            let u: f64 = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let v: f64 = j as f64 / (IMAGE_WIDTH - 1) as f64;
+            let r: Ray = Ray {origin: origin, direction: lower_left_corner + u*horizontal + v*vertical - origin};
+            let pixel_color: Color = ray_color(r);
+            write_color(pixel_color);
+        }
+    }
+
+    eprintln!("Done. ");
+}
+
+fn ray_color(r: Ray) -> Color {
+    let unit_direction: Vec3 = Vec3::unit_vector(r.direction());
+    let t = 0.5*(unit_direction.y() + 1.0);
+    (1.0-t)*Color(1.0, 1.0, 1.0) + t*Color(0.5, 0.7, 1.0)
+}
+
 fn write_color(pixel_color: Color) {
     println!("{} {} {}", 
         (255.999 * pixel_color.x()) as u64, 
@@ -216,7 +276,7 @@ fn main() {
     // let b = Vec3(2.0, 3.0, 4.0);
     // let c : Vec3 = 2.0*a;
     // println!("This is my vector: {}", c);
-    second_image();
+    third_image();
 }
 
 
