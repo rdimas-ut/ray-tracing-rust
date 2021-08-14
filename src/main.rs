e7abbb240b69aa6ab31703c1913ba5c7d3ca38e5
Ruben Dimas <57540512+rdimas-ut@users.noreply.github.com>
2021-08-14 00:08:05 -0500

    Added shading to the sphere

--- src/main.rs
+++ src/main.rs
@@ -259,18 +259,24 @@ fn third_image() {
     eprintln!("Done. ");
 }
 
-fn hit_sphere(center: Point3, radius: f64, r: Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f64, r: Ray) -> f64 {
     let oc: Vec3 = r.origin() - center;
     let a: f64 = Vec3::dot(r.direction(), r.direction());
     let b: f64 = 2.0 * Vec3::dot(oc, r.direction());
     let c: f64 = Vec3::dot(oc, oc) - radius*radius;
     let discriminant: f64 = (b*b) - (4.0*a*c);
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        return -1.0;
+    } else {
+        return (-b - discriminant.sqrt()) / (2.0*a);
+    }
 }
 
 fn ray_color(r: Ray) -> Color {
-    if hit_sphere(Point3(0.0, 0.0, 1.0), 0.5, r) {
-        return Color(1.0, 0.0, 0.0);
+    let mut t: f64 = hit_sphere(Point3(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let N: Vec3 = Vec3::unit_vector(r.at(t) - Vec3(0.0,0.0,-1.0));
+        return 0.5*Color(N.x()+1.0, N.y()+1.0, N.z()+1.0);
     }
     let unit_direction: Vec3 = Vec3::unit_vector(r.direction());
     let t = 0.5*(unit_direction.y() + 1.0);
