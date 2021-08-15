8757947ddb5a718f179ecfad441126119970f888
Ruben <rdimas@utexas.edu>
2021-08-15 01:54:22 -0500

    Implemented the hittable and hittablelist

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,19 @@ use std::ops::Add;
 use std::ops::Sub;
 use std::ops::Mul;
 use std::ops::Div;
+use std::vec::Vec;
+use std::rc::Rc;
 
 use std::fmt;
+
+// Constants
+const INFINITY: f64 = 100000000000000000.0;
+const PI: f64 = 3.1415926535897932385;
+
+// Utility Functions
+fn degrees_to_radians(degrees: f64) -> f64 {
+    (degrees*PI)/180.0
+}
 
 #[derive(Copy, Clone)]
 struct Vec3(f64, f64, f64);
@@ -181,6 +192,98 @@ impl Ray {
     }
 }
 
+#[derive(Copy, Clone)]
+struct HitRecord {
+    p: Point3,
+    normal: Vec3,
+    t: f64,
+    front_face: bool,
+}
+
+impl HitRecord {
+    fn set_face_normal(&mut self, r: Ray, outward_normal: Vec3) {
+        self.front_face = Vec3::dot(r.direction(), outward_normal) < 0.0;
+        self.normal = if self.front_face {outward_normal} else {-outward_normal};
+    }
+}
+
+trait Hittable {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool;
+}
+
+#[derive(Copy, Clone)]
+struct Sphere {
+    center: Point3,
+    radius: f64
+}
+
+impl Hittable for Sphere {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
+        let oc: Vec3 = r.origin() - self.center;
+        let a: f64 = r.direction().length_square();
+        let half_b: f64 = Vec3::dot(oc, r.direction());
+        let c: f64 = oc.length_square() - self.radius*self.radius;
+
+        let discriminant: f64 = half_b*half_b - a*c;
+        if discriminant < 0.0 {return false;}
+        let sqrtd = discriminant.sqrt();
+
+        // Find the nearest root that lies in the acceptable range
+        let mut root: f64 = (-half_b - sqrtd) / a;
+        if root < t_min || t_max < root {
+            root = (-half_b + sqrtd) / a;
+            if root < t_min || t_max < root {
+                return false;
+            }
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        rec.normal = (rec.p - self.center) / self.radius;
+        let outward_normal: Vec3 = (rec.p - self.center) / self.radius;
+        rec.set_face_normal(*r, outward_normal);
+
+        true
+    }   
+}
+
+struct HittableList{
+    objects: Vec<Rc<dyn Hittable>>,
+} 
+
+impl HittableList {
+    fn clear(&mut self) {
+        self.objects.clear()
+    }
+
+    fn add(&mut self, object: Rc<dyn Hittable>) {
+        self.objects.push(object)
+    }
+}
+
+impl Hittable for HittableList {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
+        let mut temp_rec: HitRecord = HitRecord {
+            p: Point3(0.0, 0.0, 0.0),
+            normal: Vec3(0.0, 0.0, 0.0),
+            t: 0.0,
+            front_face: false,
+        };
+        let mut hit_anything: bool = false;
+        let mut closest_so_far: f64 = t_max;
+
+        for object in self.objects.iter() {
+            if object.hit(r, t_min, closest_so_far, &mut temp_rec) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                *rec = temp_rec;
+            }
+        }
+
+        return hit_anything;
+    }
+}
+
 fn first_image() {
     // Image
     const IMAGE_WIDTH: u32 = 256;
@@ -261,19 +364,19 @@ fn third_image() {
 
 fn hit_sphere(center: Point3, radius: f64, r: Ray) -> f64 {
     let oc: Vec3 = r.origin() - center;
-    let a: f64 = Vec3::dot(r.direction(), r.direction());
-    let b: f64 = 2.0 * Vec3::dot(oc, r.direction());
-    let c: f64 = Vec3::dot(oc, oc) - radius*radius;
-    let discriminant: f64 = (b*b) - (4.0*a*c);
+    let a: f64 = r.direction().length_square();
+    let half_b: f64 = Vec3::dot(oc, r.direction());
+    let c: f64 = oc.length_square() - radius*radius;
+    let discriminant: f64 = (half_b*half_b) - (a*c);
     if discriminant < 0.0 {
         return -1.0;
     } else {
-        return (-b - discriminant.sqrt()) / (2.0*a);
+        return (-half_b - discriminant.sqrt()) / a;
     }
 }
 
 fn ray_color(r: Ray) -> Color {
-    let mut t: f64 = hit_sphere(Point3(0.0, 0.0, -1.0), 0.5, r);
+    let t: f64 = hit_sphere(Point3(0.0, 0.0, -1.0), 0.5, r);
     if t > 0.0 {
         let N: Vec3 = Vec3::unit_vector(r.at(t) - Vec3(0.0,0.0,-1.0));
         return 0.5*Color(N.x()+1.0, N.y()+1.0, N.z()+1.0);
