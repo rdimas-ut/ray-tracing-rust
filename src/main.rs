0636f39d9009efe2b3ae4fdf7d35883ed8b92e5f
Ruben Dimas <57540512+rdimas-ut@users.noreply.github.com>
2021-08-20 19:07:38 -0500

    Final render made

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,9 @@ use hittable_list::HittableList;
 mod sphere;
 use sphere::Sphere;
 
+mod rtweekend;
+use rtweekend::random_double;
+
 use std::vec::Vec;
 
 use std::rc::Rc;
@@ -33,23 +36,10 @@ use std::cell::RefCell;
 
 use rand::Rng;
 use rand::distributions::Uniform;
-
-use std::default::Default;
 use std::f64::MAX;
 
 // Constants
 const INFINITY: f64 = MAX;
-
-// Utility Functions
-fn random_double() -> f64 {
-    let a: f64 = rand::thread_rng().gen_range(0.0..1.0);
-    a
-}
-
-fn random_double_range(min: f64, max: f64) -> f64 {
-    let a: f64 = rand::thread_rng().gen_range(0.0..1.0);
-    min + (max - min)*a
-}
 
 
 fn clamp(x: f64, min: f64, max: f64) -> f64 {
@@ -117,43 +107,78 @@ fn write_color(pixel_color: Color, samples_per_pixel: u64) {
         (256.0 * clamp(b, 0.0, 0.999)) as u64)
 }
 
+fn random_scene(zero_to_one: rand::distributions::Uniform<f64>) -> HittableList {
+    let mut world: HittableList = HittableList { objects: Vec::new() };
+    let zero_to_five_tenths_dist = Uniform::new(0.0f64, 0.5f64);
+    let five_tenths_to_one_dist = Uniform::new(0.5f64, 1.0f64);
 
+    let ground_material = Rc::new(RefCell::new(Lambertian{ albedo: Color(0.5, 0.5, 0.5) }));
+    world.add(Rc::new(RefCell::new(Sphere { center: Point3(0.0, -1000.0, 0.0), radius: 1000.0, mat_ptr: ground_material })));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat: f64 = random_double(zero_to_one);
+            let center: Point3 = Point3(a as f64 + 0.9*random_double(zero_to_one), 0.2, b as f64 + 0.9*random_double(zero_to_one));
+
+            if (center - Point3(4.0, 0.2, 0.0)).length() > 0.9 {
+                let _sphere_material = Rc::new(RefCell::new(Lambertian{ albedo: Color(0.8, 0.8, 0.0) }));
+
+                if choose_mat < 0.8 {
+                    // diffuse
+                    let albedo: Color = Color::random() * Color::random();
+                    let sphere_material = Rc::new(RefCell::new(Lambertian{ albedo: albedo }));
+                    world.add(Rc::new(RefCell::new(Sphere { center: center, radius: 0.2, mat_ptr: sphere_material })));
+                } else if choose_mat < 0.95 {
+                    // metal
+                    let albedo = Color::random_range(0.5, 1.0);
+                    let fuzz = random_double(zero_to_five_tenths_dist);
+                    let sphere_material = Rc::new(RefCell::new(Metal{ albedo: albedo, fuzz: fuzz }));
+                    world.add(Rc::new(RefCell::new(Sphere { center: center, radius: 0.2, mat_ptr: sphere_material })));
+                } else {
+                    // glass
+                    let sphere_material = Rc::new(RefCell::new(Dialectric{ ir: 1.5 }));
+                    world.add(Rc::new(RefCell::new(Sphere { center: center, radius: 0.2, mat_ptr: sphere_material })));
+                }
+            }
+        }
+    }
+
+    
+    let material1 = Rc::new(RefCell::new(Dialectric{ ir: 1.5 }));
+    world.add(Rc::new(RefCell::new(Sphere { center: Point3(0.0, 1.0, 0.0), radius: 1.0, mat_ptr: material1 })));
+
+    let material2 = Rc::new(RefCell::new(Lambertian{ albedo: Color(0.4, 0.2, 0.1) }));
+    world.add(Rc::new(RefCell::new(Sphere { center: Point3(-4.0, 1.0, 0.0), radius: 1.0, mat_ptr: material2 })));
+
+    let material3 = Rc::new(RefCell::new(Metal{ albedo: Color(0.7, 0.6, 0.5), fuzz: 0.0 }));
+    world.add(Rc::new(RefCell::new(Sphere { center: Point3(4.0, 1.0, 0.0), radius: 1.0, mat_ptr: material3 })));
+
+    return world;
+}
 
 fn main() {
+        // RNG
+        let zero_to_one = Uniform::new(0.0f64, 1.0f64);
+        let mut rng  = rand::thread_rng();
+
         // Image
-        const ASPECT_RATIO: f64 = 16.0/9.0;
-        const IMAGE_WIDTH: u64 = 400;
+        const ASPECT_RATIO: f64 = 3.0/2.0;
+        const IMAGE_WIDTH: u64 = 1200;
         const IMAGE_HEIGTH: u64 = (IMAGE_WIDTH as f64/ASPECT_RATIO) as u64;
-        const SAMPLES_PER_PIXEL: u64 = 100;
+        const SAMPLES_PER_PIXEL: u64 = 500;
         const MAX_DEPTH: u64 = 50;
     
         // World
-        let R: f64 = (PI/4.0).cos();
-        let mut world: HittableList = HittableList { objects: Vec::new() };
-    
-        let material_ground = Rc::new(RefCell::new(Lambertian{ albedo: Color(0.8, 0.8, 0.0) }));
-        let material_center = Rc::new(RefCell::new(Lambertian{ albedo: Color(0.1, 0.2, 0.5) }));
-        let material_left   = Rc::new(RefCell::new(Dialectric {ir: 1.5 }));
-        let material_left2   = Rc::new(RefCell::new(Dialectric {ir: 1.5 }));
-        let material_right  = Rc::new(RefCell::new(Metal {albedo: Color(0.8, 0.6, 0.2), fuzz: 0.0 }));
-    
-        world.add(Rc::new(RefCell::new(Sphere { center: Point3(0.0, -100.5, -1.0), radius: 100.0, mat_ptr: material_ground })));
-        world.add(Rc::new(RefCell::new(Sphere { center: Point3(0.0, 0.0, -1.0), radius: 0.5, mat_ptr: material_center })));
-        world.add(Rc::new(RefCell::new(Sphere { center: Point3(-1.0, 0.0, -1.0), radius: 0.5, mat_ptr: material_left })));
-        world.add(Rc::new(RefCell::new(Sphere { center: Point3(-1.0, 0.0, -1.0), radius: -0.4, mat_ptr: material_left2 })));
-        world.add(Rc::new(RefCell::new(Sphere { center: Point3(1.0, 0.0, -1.0), radius: 0.5, mat_ptr: material_right })));
-    
+        let mut world = random_scene(zero_to_one);
+
         // Camera
-        let lookfrom: Point3 = Point3(3.0, 3.0, 2.0);
-        let lookat: Point3 = Point3(0.0, 0.0, -1.0);
+        let lookfrom: Point3 = Point3(13.0, 2.0, 3.0);
+        let lookat: Point3 = Point3(0.0, 0.0, 0.0);
         let vup: Vec3 = Vec3(0.0, 1.0, 0.0);
-        let dist_to_focus: f64 = (lookfrom-lookat).length();
-        let aperture: f64 = 2.0;
+        let dist_to_focus: f64 = 10.0;
+        let aperture: f64 = 0.1;
+
         let cam: Camera = Camera::new(lookfrom, lookat, vup, 20.0, ASPECT_RATIO, aperture, dist_to_focus); 
-    
-        // RNG
-        let random_space = Uniform::new(0.0f64, 1.0f64);
-        let mut rng  = rand::thread_rng();
     
         // Render
         print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGTH);
@@ -163,8 +188,8 @@ fn main() {
             for i in 0..IMAGE_WIDTH {
                 let mut pixel_color: Color = Color(0.0, 0.0, 0.0);
                 for _k in 0..SAMPLES_PER_PIXEL {
-                    let u: f64 = (i as f64 + rng.sample(random_space)) / (IMAGE_WIDTH - 1) as f64;
-                    let v: f64 = (j as f64 + rng.sample(random_space)) / (IMAGE_HEIGTH - 1) as f64;
+                    let u: f64 = (i as f64 + rng.sample(zero_to_one)) / (IMAGE_WIDTH - 1) as f64;
+                    let v: f64 = (j as f64 + rng.sample(zero_to_one)) / (IMAGE_HEIGTH - 1) as f64;
                     let r: Ray = cam.get_ray(u, v);
                     pixel_color += ray_color(&r, &mut world, MAX_DEPTH);
                 }
