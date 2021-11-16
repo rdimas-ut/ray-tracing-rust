1a7643b7b7382cfb6b5be19585627195fbb4bc6a
Ruben Dimas <57540512+rdimas-ut@users.noreply.github.com>
2021-11-15 23:55:36 -0600

    working on reading images as textures

--- src/texture.rs
+++ src/texture.rs
@@ -6,6 +6,9 @@ use crate::perlin::Perlin;
 use std::rc::Rc;
 use std::cell::RefCell;
 
+use image::io::Reader;
+use image::ImageBuffer;
+
 pub trait Texture {
     fn value(&self, u: f64, v: f64, p: &Point3) -> Color;
 }
@@ -61,5 +64,43 @@ pub struct NoiseTexture {
 impl Texture for NoiseTexture {
     fn value(&self, u: f64, v: f64, p: &Point3) -> Color {
         Color(1.0, 1.0, 1.0) * 0.5 * (1.0 + (self.scale*p.z() + 10.0*self.noise.turb(p)).sin())
+    }
+}
+
+pub struct ImageTexture {
+    data: ImageBuffer<image::Rgb<u8>, std::vec::Vec<u8>>,
+    width: u32,
+    height: u32,
+    bytes_per_scanline: u32,
+}
+
+impl Texture for ImageTexture {
+    fn value(&self, u: f64, v: f64, p: &Point3) -> Color {
+        
+
+        Color(0.0, 0.0, 0.0)
+    }
+}
+
+impl ImageTexture {
+    fn new(filename: String) -> Self {
+        let bytes_per_pixel: u32 = 3;
+        let limage = Reader::open(filename).unwrap().decode().unwrap().as_rgb8().clone();
+
+        let (width, height) = limage.unwrap().dimensions();
+
+        // if (!data) {
+        //     std::cerr << "ERROR: Could not load texture image file '" << filename << "'.\n";
+        //     width = height = 0;
+        // }
+
+        let bytes_per_scanline = bytes_per_pixel*width;
+        
+        ImageTexture {
+            data: limage,
+            width: width,
+            height: height,
+            bytes_per_scanline: bytes_per_scanline
+        }
     }
 }
