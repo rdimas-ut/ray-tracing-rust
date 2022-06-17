179668f0dd231a64eb7fe2d04204561aaaee2892
Ruben Dimas <rdimas@utexas.edu>
2022-06-16 22:37:34 -0500

    finished up to chapter 6

---
+++ src/bin/sphere_importance.rs
@@ -0,0 +1,17 @@
+use ray_tracing_rust::{rtweekend::random_double_range, vec3::{Vec3, random_unit_vector}};
+use std::f64::consts::PI;
+
+fn pdf(p: &Vec3) -> f64{
+    1.0/(4.0*PI)
+}
+
+fn main() {
+    let n: u64 = 1000000;
+    let mut sum = 0.0;
+    for _i in 0..n {
+        let d: Vec3 = random_unit_vector();
+        let cosine_squared: f64 = d.z()*d.z();
+        sum += cosine_squared/pdf(&d);
+    }
+    println!("I = {}", (sum/n as f64));
+}
